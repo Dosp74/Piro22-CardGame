d0f99c22f887a92b6ff972ca5b4b45d379682248
HAN JONGSEO <pantene74@naver.com>
2025-01-16 21:26:16 +0900

    feat: 게임 정보 페이지(=게임 detail 페이지) - 4-1, 4-2, 4-3 기본 틀

--- game/urls.py
+++ game/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('start/', views.start_game, name='start_game'),
+    path('game/<int:game_id>/', views.game_detail, name='game_detail'),
 ]
--- game/views.py
+++ game/views.py
@@ -14,3 +14,19 @@ def start_game(request):
         cards = generate_random_cards() #5개의 카드 리스트
         users = Account.objects.exclude(id=request.user.id)  # 상대 유저 리스트
         return render(request, "game/start_game.html", {"cards": cards, "users": users})
+
+@login_required
+def game_detail(request, game_id):
+    # 게임 정보 가져오기
+    game = get_object_or_404(Game, id=game_id)
+    
+    # 로그인한 사용자의 관점에서 게임 상태 확인
+    if game.result == "":
+        if request.user == game.attacker:
+            context = {"game": game, "status": "waiting"}  # 진행 중 (4-1 상황)
+        elif request.user == game.defender:
+            context = {"game": game, "status": "counterattack"}  # 반격 가능 (4-2 상황)
+    else:
+        context = {"game": game, "status": "finished"}  # 종료 (4-3 상황)
+
+    return render(request, "game/game_detail.html", context)
