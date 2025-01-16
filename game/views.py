cf62fe8c5e303bcb8f1df79d2265571c67779353
dhrkawk <jajwon202@naver.com>
2025-01-16 17:31:31 +0900

    add: game models urls views start_game.html

--- game/views.py
+++ game/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+import random
+from django.shortcuts import render, get_object_or_404, redirect
+from django.contrib.auth.decorators import login_required
+from .models import Game
+from account.models import Account
 
-# Create your views here.
+def start_game(request):
+    def generate_random_cards():
+        #1~10 중 5개의 랜덤한 숫자를 반환
+        return random.sample(range(1, 11), 5)
+
+    if request.method == "GET":
+        # 5장의 랜덤 카드를 생성
+        cards = generate_random_cards() #5개의 카드 리스트
+        users = Account.objects.exclude(id=request.user.id)  # 상대 유저 리스트
+        return render(request, "game/start_game.html", {"cards": cards, "users": users})
