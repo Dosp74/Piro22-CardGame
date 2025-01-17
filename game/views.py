979464c599d95e58b7fc57441800ebedf3b37bbc
dhrkawk <jajwon202@naver.com>
2025-01-17 14:14:41 +0900

    add: game counterattack, game_detail

--- game/views.py
+++ game/views.py
@@ -2,7 +2,7 @@ import random
 from django.shortcuts import render, get_object_or_404, redirect
 from django.contrib.auth.decorators import login_required
 from .models import Game
-from account.models import Account
+#from account.models import Account
 
 def start_game(request):
     def generate_random_cards():
@@ -12,8 +12,8 @@ def start_game(request):
     if request.method == "GET":
         # 5장의 랜덤 카드를 생성
         cards = generate_random_cards() #5개의 카드 리스트
-        users = Account.objects.exclude(id=request.user.id)  # 상대 유저 리스트
-        return render(request, "game/start_game.html", {"cards": cards, "users": users})
+        #users = Account.objects.exclude(id=request.user.id)  # 상대 유저 리스트
+        return render(request, "game/start_game.html", {"cards": cards})
 
 @login_required
 def game_detail(request, game_id):
