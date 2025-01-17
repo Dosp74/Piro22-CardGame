919d96345d961c0b6d4d705b980180f9e001d2f7
leexzu1 <jackie20029655@gmail.com>
2025-01-17 19:41:28 +0900

    feat:다같이 pull 받아요

--- user/models.py
+++ user/models.py
@@ -1,10 +1,22 @@
 # user/models.py
 from django.db import models
-from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import AbstractUser, Group, Permission
 
 class User(AbstractUser):
     username = models.CharField(max_length=50, unique=True)  # 유저 이름
     point = models.IntegerField(default=0)  # 유저 점수
+
+    # groups와 user_permissions 필드에 대해 related_name 설정 추가
+    groups = models.ManyToManyField(
+        Group,
+        related_name='custom_user_groups',  # related_name을 'custom_user_groups'로 설정
+        blank=True
+    )
+    user_permissions = models.ManyToManyField(
+        Permission,
+        related_name='custom_user_permissions',  # related_name을 'custom_user_permissions'로 설정
+        blank=True
+    )
 
     def __str__(self):
         return f"{self.username} ({self.point} points)"
